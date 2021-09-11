ca17b30f8eefc2a68f48189822a993d7a9b3c9e8
Tarrun <52660661+tarrun99@users.noreply.github.com>
2021-09-11 08:42:22 -0400

    macchanger.py

--- macchangepy.py
+++ macchangepy.py
@@ -2,6 +2,11 @@
 import subprocess
 import optparse
 
+def change(interface, new_mac):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest = "interface", help = "INTERFACE TO CHANGE")
@@ -12,6 +17,5 @@ parser.add_option("-m","--new_mac", dest = "new_mac", help = "NEW MAC ADDRESS")
 interface = options.interface
 new_mac = options.new_mac
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether",new_mac ])
-subprocess.call(["ifconfig", interface, "up"])
+change(interface, new_mac);
+
